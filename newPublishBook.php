956419160c7e8de0af71536cf00d498ca86f1578
2b0x <1315181411@qq.com>
2017-12-28 17:35:15 +0800

    实现ajax多图传

---
+++ newPublishBook.php
@@ -0,0 +1,35 @@
+<?php
+		require('conn.php');
+		$book_name = isset($_POST['book_name'])? $_POST['book_name'] : '';  
+		$author = isset($_POST['author'])? $_POST['author'] : '';
+		$price = isset($_POST['price'])? $_POST['price'] : '';  
+		$isbn = isset($_POST['isbn'])? $_POST['isbn'] : '';  
+		$press = isset($_POST['press'])? $_POST['press'] : '';  
+		$intro = isset($_POST['intro'])? $_POST['intro'] : ''; 
+		$nameTag = time();
+		$filename1 = $nameTag . '0' . substr($_FILES['photo1']['name'], strrpos($_FILES['photo1']['name'],'.'));  
+		$filename2 = $nameTag . '1' . substr($_FILES['photo2']['name'], strrpos($_FILES['photo2']['name'],'.'));  
+		$filename3 = $nameTag . '2' . substr($_FILES['photo3']['name'], strrpos($_FILES['photo3']['name'],'.'));  
+		$response = array();
+		$path1 = "img/"  . $filename1;
+		$path2 = "img/"  . $filename2;
+		$path3 = "img/"  . $filename3;
+		if(move_uploaded_file($_FILES['photo1']['tmp_name'], $path1) && move_uploaded_file($_FILES['photo2']['tmp_name'], $path2) && move_uploaded_file($_FILES['photo3']['tmp_name'], $path3) ){
+			
+			  
+		    $response['isSuccess'] = true;  
+		    $response['book_name'] = $book_name;  
+		    $response['author'] = $author;
+		    $response['price'] = $price;  
+		    $response['isbn'] = $isbn;  
+		    $response['press'] = $press;  
+		    $response['intro'] = $intro;  
+		    $response['photo1'] = $path1; 
+		    $response['photo2'] = $path2; 
+		    $response['photo3'] = $path3; 
+			 
+		}else{  
+		    $response['isSuccess'] = false;  
+		}  
+		echo json_encode($response);
+?>
